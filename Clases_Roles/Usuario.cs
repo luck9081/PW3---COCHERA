c0555cf1c69bf4cbca772e29097d62ebcfc29275
LucioWQ <glados_s4@hotmail.com>
2016-10-14 15:03:41 -0300

    enter

--- Clases_Roles/Usuario.cs
+++ Clases_Roles/Usuario.cs
@@ -8,6 +8,9 @@ namespace Clases_Roles
 {
     public class Usuario
     {
+
+
+
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Email { get; set; }
