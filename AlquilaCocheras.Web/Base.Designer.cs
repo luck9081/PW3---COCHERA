830a2535995f6bb12a7977af44538ccf08d18b16
luck9081 <luck_m_r@hotmail.com>
2016-10-18 16:24:43 -0300

    Revert "Base de datos"
    This reverts commit 9635c86889605dcdfe95e78fad0a2ab557521c56.

--- AlquilaCocheras.Web/Base.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Usuario\Documents\GitHub\PW3---COCHERA\AlquilaCocheras.Web\Base.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
