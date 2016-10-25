fa2a989266792c64ed9918ac401bbdfb4b3ee00d
luck9081 <luck_m_r@hotmail.com>
2016-10-25 17:10:02 -0300

    Creación "Acceso_BaseDatos"
    Creación del proyecto que contendrá el .edmx
    
    Creación de clase "Cochera", y codificación de la misma.

--- Clases_Roles/Usuario.cs
+++ Clases_Roles/Usuario.cs
@@ -3,12 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Acceso_Datos;
+using Acceso_BaseDatos;
+
 namespace Clases_Roles
 {
     public class Usuario
     {
-            
+        TP_20162CEntities ctx;
+        public Usuario(TP_20162CEntities contexto)
+        {
+            ctx = contexto;
+        }
+        public void agregarUsuario(Usuarios us)
+        {
+            ctx.Usuarios.Add(us);
+            ctx.SaveChanges();
+        }
 
+        public bool encontrarUsuario(string mail)
+        {
+            Usuarios resultado = (from u in ctx.Usuarios
+                                  where u.Email == mail
+                                  select u).FirstOrDefault();
+            if (resultado != null)
+                return true;
+            else
+                return false;
+        
+        }
+
+        /* ------------------------------ BÚSQUEDA DE USUARIO ------------------------------ */
+        // Por ID
+        public Usuarios obtenerUsuario(int id)
+        {
+            Usuarios usuario = ctx.Usuarios.First(u => u.IdUsuario == id);
+            return usuario;
+        }
+        //Por Email
+        public Usuarios obtenerUsuario(string mail)
+        {
+            Usuarios usuario = ctx.Usuarios.First(u => u.Email == mail);
+            return usuario;
+        }
+        public bool compararContraseña(string mail, string pass)
+        {
+            Usuarios usuario = obtenerUsuario(mail);
+            if (usuario.Contrasenia == pass)
+                return true;
+            else
+                return false;
+
+        }
     }
 }
